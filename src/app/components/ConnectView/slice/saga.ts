ceef7f315c509b39cd1a9846a086d89f70895298
brady <brady@realmsforever.com>
2023-10-12 20:11:07 -0500

    Allow user to type the 12 word recovery phrase

--- src/app/components/ConnectView/slice/saga.ts
+++ src/app/components/ConnectView/slice/saga.ts
@@ -20,46 +20,41 @@ import * as bip39 from 'bip39';
 import { selectPathBase } from './selectors';
 
 export function* triggerGenerateProfileRequest(action) {
-    const pathBase = yield select(selectPathBase);
-    console.log('pathBase', pathBase);
-    const primaryPath = `${pathBase}/0/0`;
-    const fundingPath = `${pathBase}/1/0`;
-    const mnemonic = createMnemonicPhrase();
-    const primary: CreateKeyPairInterface = yield generateKeysFromPhrase(
-        mnemonic.phrase,
-        primaryPath,
-    );
-    const funding: CreateKeyPairInterface = yield generateKeysFromPhrase(
-        mnemonic.phrase,
-        fundingPath,
-    );
+  const pathBase = yield select(selectPathBase);
+  const primaryPath = `${pathBase}/0/0`;
+  const fundingPath = `${pathBase}/1/0`;
+  const mnemonic = createMnemonicPhrase();
+  const primary: CreateKeyPairInterface = yield generateKeysFromPhrase(
+    mnemonic.phrase,
+    primaryPath
+  );
+  const funding: CreateKeyPairInterface = yield generateKeysFromPhrase(
+    mnemonic.phrase,
+    fundingPath
+  );
 
-    yield put(actions.setGenerateProfile({ phrase: mnemonic.phrase, primary, funding }));
+  yield put(actions.setGenerateProfile({ phrase: mnemonic.phrase, primary, funding }));
 }
 
 export function* triggerChangePhraseRequest(action) {
-    const pathBase = yield select(selectPathBase);
-    console.log('pathBase', pathBase);
-    const isValid = bip39.validateMnemonic(action.payload.phrase);
-    if (isValid) {
-        const primaryPath = `${pathBase}/0/0`;
-        const fundingPath = `${pathBase}/1/0`;
-        const mnemonic = action.payload.phrase;
-        const primary: CreateKeyPairInterface = yield generateKeysFromPhrase(mnemonic, primaryPath);
-        const funding: CreateKeyPairInterface = yield generateKeysFromPhrase(mnemonic, fundingPath);
-        yield put(actions.setChangePhrase({ phrase: mnemonic, primary, funding }));
-    }
+  const pathBase = yield select(selectPathBase);
+  const primaryPath = `${pathBase}/0/0`;
+  const fundingPath = `${pathBase}/1/0`;
+  const mnemonic = action.payload.phrase;
+  const primary: CreateKeyPairInterface = yield generateKeysFromPhrase(mnemonic, primaryPath);
+  const funding: CreateKeyPairInterface = yield generateKeysFromPhrase(mnemonic, fundingPath);
+  yield put(actions.setChangePhrase({ phrase: mnemonic, primary, funding }));
 }
 
 /**
  * Root saga manages watcher lifecycle
  */
 export function* getConnectSaga() {
-    // Watches for loadRepos actions and calls getRepos when one comes in.
-    // By using `takeLatest` only the result of the latest API call is applied.
-    // It returns task descriptor (just like fork) so we can continue execution
-    // It will be cancelled automatically on component unmount
-    yield takeLatest(actions.triggerGenerateProfile.type, triggerGenerateProfileRequest);
+  // Watches for loadRepos actions and calls getRepos when one comes in.
+  // By using `takeLatest` only the result of the latest API call is applied.
+  // It returns task descriptor (just like fork) so we can continue execution
+  // It will be cancelled automatically on component unmount
+  yield takeLatest(actions.triggerGenerateProfile.type, triggerGenerateProfileRequest);
 
-    yield takeLatest(actions.triggerChangePhrase.type, triggerChangePhraseRequest);
+  yield takeLatest(actions.triggerChangePhrase.type, triggerChangePhraseRequest);
 }
